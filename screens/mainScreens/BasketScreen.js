51188cdf552fcbce742ab90b1665e253bea76a29
hly1013 <hly10132@gmail.com>
2020-09-13 21:32:43 +0900

    기본 틀

--- screens/mainScreens/BasketScreen.js
+++ screens/mainScreens/BasketScreen.js
@@ -1,14 +1,61 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const SelectMenuButton = ({navigation}) => {
+    return(
+        <TouchableOpacity activeOpacity={0.8} 
+        style={styles.button} 
+        onPress={() => navigation.navigate("Order")}>  
+            <Text style={styles.text}>메뉴 담기</Text>
+        </TouchableOpacity>
+    );
+};
 
 const BasketScreen = (props) => {
     return (
         <View>
-            <Text>This is BasketScreen</Text>
-            <Button title="메뉴 담기(OrderScreen)" onPress={() => props.navigation.navigate("Order")}></Button>
+            <SelectMenuButton navigation={props.navigation}/>
         </View>
     );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    top: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    mid: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    bottom: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    button: {
+        width: "100%",
+        height: 110,
+        backgroundColor: "orange",
+        justifyContent: "center",
+        alignItems: "center",
+        position: 'absolute',
+        bottom: 0
+    },
+    touched: {
+        backgroundColor: "#ee5555"
+    },
+    text: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+});
 
 export default BasketScreen;
--- screens/mainScreens/OrderScreen.js
+++ screens/mainScreens/OrderScreen.js
@@ -1,14 +1,61 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const OrderButton = ({navigation}) => {
+    return(
+        <TouchableOpacity activeOpacity={0.8} 
+        style={styles.button} 
+        onPress={() => navigation.navigate("FinishOrder")}>  
+            <Text style={styles.text}>주문하기</Text>
+        </TouchableOpacity>
+    );
+};
 
 const OrderScreen = (props) => {
     return (
         <View>
-            <Text>This is OrderScreen</Text>
-            <Button title="주문하기(Go to Finish Order Screen)" onPress={() => props.navigation.navigate("FinishOrder")}></Button>
+            <OrderButton navigation={props.navigation}/>
         </View>
     );
 };
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "black"
+    },
+    top: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    mid: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    bottom: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    button: {
+        width: "100%",
+        height: 110,
+        backgroundColor: "orange",
+        justifyContent: "center",
+        alignItems: "center",
+        position: 'absolute',
+        bottom: 0
+    },
+    touched: {
+        backgroundColor: "#ee5555"
+    },
+    text: {
+        color: "#fff",
+        fontSize: 20,
+        fontWeight: "bold"
+    }
+});
 
 export default OrderScreen;
