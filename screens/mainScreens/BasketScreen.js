ff0a979f20f5050f4df689f81da8af3e61f58492
hly1013 <hly10132@gmail.com>
2020-09-15 16:37:20 +0900

    ui 수정

--- screens/mainScreens/BasketScreen.js
+++ screens/mainScreens/BasketScreen.js
@@ -55,7 +55,10 @@ class Menu extends React.Component {
 
 const Menu = ({name, cost}) => {
     return(
-        <Text style={styles.text}>{name}: {cost}</Text>
+        <TouchableOpacity style={styles.menu}>
+            <Text style={styles.text}>{name}: {cost}</Text>
+        </TouchableOpacity>
+
     );
 };
 
@@ -88,11 +91,12 @@ const BasketScreen = (props) => {
                 <Text style={styles.text}>ABC 레스토랑</Text>
             </View>
             <View style={styles.mid}>
-                <View style={{flex: 1}}>
+                <View style={styles.mid}>
                     <Text style={styles.text}>메인 메뉴</Text>
                     <Menu name="된장찌개" cost={5500}/>
+                    <Menu name="김치찌개" cost={6000}/>
                 </View>
-                <View style={{flex: 1}}>
+                <View style={styles.mid}>
                     <Text style={styles.text}>오직 와글에서만 무료!</Text>
                     <Text style={styles.text}>사이드 메뉴</Text>
                     
@@ -133,6 +137,13 @@ const styles = StyleSheet.create({
         alignItems: "center",
         position: 'absolute',
         bottom: 0
+    },
+    menu: {
+        width: 150,
+        height: 45,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "green"
     },
     touched: {
         backgroundColor: "#ee5555"
