ca1637c3f51c78d808a607c55dc7ba6a762b7c55
hly1013 <hly1013@naver.com>
2020-09-22 11:13:15 +0900

    stamp관련 js파일 생성

---
+++ screens/settingsScreens/StampboxScreen.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+
+import Colors from "../../constants/Colors";
+import CommonStyles from "../../constants/CommonStyles";
+import { headerOptions } from "../../constants/Options";
+
+const StampboxScreen = (props) => {
+    props.navigation.setOptions({
+        ...headerOptions,
+        headerTintColor: Colors.text_grey,
+        headerStyle: {
+            ...headerOptions.headerStyle,
+            backgroundColor: Colors.mid_grey,
+        },
+        headerTitleStyle: {
+            ...headerOptions.headerTitleStyle,
+            color: "white",
+        },
+    });
+    
+    return (
+
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default StampboxScreen;
