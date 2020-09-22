9ae5fcd49c59dcd882a893d2f2baa82784f1b02f
hly1013 <hly1013@naver.com>
2020-09-22 11:36:07 +0900

    stampboxscreen 대략

--- screens/settingsScreens/StampboxScreen.js
+++ screens/settingsScreens/StampboxScreen.js
@@ -5,6 +5,13 @@ import Colors from "../../constants/Colors";
 import CommonStyles from "../../constants/CommonStyles";
 import { headerOptions } from "../../constants/Options";
 
+import CardButton from "../../components/CardButton";
+
+const stampDatas = [
+    { name: "ABC레스토랑", collected: 2, all: 10},
+    { name: "가나다레스토랑", collected: 1, all: 10}
+]
+
 const StampboxScreen = (props) => {
     props.navigation.setOptions({
         ...headerOptions,
@@ -18,9 +25,20 @@ const StampboxScreen = (props) => {
             color: "white",
         },
     });
-    
-    return (
 
+    return (
+        <View style={styles.body}>
+            <Text style={styles.exp_text}>클릭시 상세 정보를 알 수 있어요.</Text>
+            <FlatList
+                key="_"
+                data={stampDatas}
+                renderItem={({ item }) => {
+                    return <CardButton name={item.storeName} collected={item.collected.toString()} all={item.all.toString()}></CardButton>;
+                }}
+                keyExtractor={(item, index) => index.toString()}
+                style={{ width: "100%" }}
+            ></FlatList>
+        </View>
     );
 }
 
