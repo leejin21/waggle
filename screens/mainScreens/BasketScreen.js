ce4e9f93b5420ec3a584a174c4169d2b953d3c50
hly1013 <hly1013@naver.com>
2020-09-24 12:45:48 +0900

    basket; sidemenu box

--- screens/mainScreens/BasketScreen.js
+++ screens/mainScreens/BasketScreen.js
@@ -6,7 +6,6 @@ import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import Colors from "../../constants/Colors";
 import CommonStyles from "../../constants/CommonStyles";
 import Card from "../../components/Card";
-import BottomButton from "../../components/BottomButton";
 
 import NoCardTemplate from "../../templates/NoCardTemplate";
 
@@ -36,14 +35,14 @@ class Menu extends Component {
 
 const BasketView = (props) => {
     return(
-        <View style={styles.mid}>
-            <View style={styles.mid}>
+        <View style={{width:"100%", height: "100%"}}>
+            <View style={{flex: 5, width: "100%"}}>
                 <Text style={[CommonStyles.bold_text, {color: "white"}]}>메인 메뉴</Text>
                 <Menu name="된장찌개" cost={5500}/>
                 <Menu name="김치찌개" cost={6000}/>
             </View>
-            <View style={styles.mid}>
-                <Card>
+            <View style={{flex: 4, width: "100%"}}>
+                <Card style={{width: "94%", height: "100%", marginTop: 0, padding: 10, marginBottom: 20, marginHorizontal: 13}}>
                     <Text style={[CommonStyles.bold_text, {color: Colors.deep_yellow}]}>오직 와글에서만 무료!</Text>
                     <Text style={[CommonStyles.bold_text, {color: "white"}]}>사이드 메뉴</Text>
                 </Card>
@@ -82,7 +81,7 @@ const BasketScreen = (props) => {
         navigation={props.navigation}
         toWhere={"Order"}
         data={menu}
-        isHeaderBlack={true}
+        isHeaderBlack={false}
         />
     );
 }
