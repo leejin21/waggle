076e7be1966852baca2fecdb333deea5f57536cc
hly1013 <hly1013@naver.com>
2020-09-24 17:23:44 +0900

    basketscreen 대강 완료

--- screens/mainScreens/BasketScreen.js
+++ screens/mainScreens/BasketScreen.js
@@ -12,10 +12,9 @@ import CheckCircle from "../../components/CheckCircle";
 import NoCardTemplate from "../../templates/NoCardTemplate";
 
 const circle_size = 93;
+const padding_size = 40;
 
-// stamptocoupon의 circle_check/uncheck 활용
-// usestate도 같이 써서
-
+// check가 되면 왜 왼쪽으로 가냐
 const Circle_check = () => {
     return(
         <CheckCircle SIZE={circle_size} touchable={false}></CheckCircle>
@@ -23,7 +22,7 @@ const Circle_check = () => {
 }
 const Circle_uncheck = () => {
     return(
-        <TouchableHighlight style={{...styles.circle, backgroundColor:"black"}}/>
+        <TouchableHighlight style={{...styles.circle, backgroundColor:"white", marginRight: padding_size}}/>
     );
 }
 
@@ -43,8 +42,7 @@ class Menu extends Component {
 
     render(){
         return(
-            <TouchableOpacity
-            onPress={() => this.handleClick(this.state)}>
+            <TouchableOpacity onPress={() => this.handleClick(this.state)} style={{width: circle_size + padding_size, justifyContent:"flex-start", alignItems:"flex-start"}}>
                 {this.state.selected? <Circle_check/>:<Circle_uncheck/>}
                 <Text style={CommonStyles.small_text}>{this.props.name}: {this.props.cost}</Text>
             </TouchableOpacity>
@@ -55,16 +53,25 @@ class Menu extends Component {
 const BasketView = (props) => {
     return(
         <View style={{width:"100%", height: "100%"}}>
-            <View style={{flex: 5, width: "100%"}}>
-                <Text style={[CommonStyles.bold_text, {color: "white"}]}>메인 메뉴</Text>
-                <View style={styles.border}/>
-                <Menu name="된장찌개" cost={5500}/>
-                <Menu name="김치찌개" cost={6000}/>
+            <View style={{flex: 5, width: "100%", paddingHorizontal: 20}}>
+                <View style={{...styles.title_view, flex: 1}}>
+                    <Text style={[CommonStyles.bold_text, {color: "white"}]}>메인 메뉴</Text>
+                </View>
+                <View style={{...styles.menu_view, flex: 7}}>
+                    <Menu name="된장찌개" cost={5500}/>
+                    <Menu name="김치찌개" cost={6000}/>
+                </View>
             </View>
             <View style={{flex: 4, width: "100%"}}>
                 <Card style={{width: "94%", height: "100%", marginTop: 0, padding: 10, marginBottom: 20, marginHorizontal: 13}}>
-                    <Text style={[CommonStyles.bold_text, {color: Colors.deep_yellow}]}>오직 와글에서만 무료!</Text>
-                    <Text style={[CommonStyles.bold_text, {color: "white"}]}>사이드 메뉴</Text>
+                    <View style={{...styles.title_view, flex:1}}>
+                        <Text style={[CommonStyles.bold_text, {color: Colors.deep_yellow}]}>오직 와글에서만 무료!</Text>
+                        <Text style={[CommonStyles.bold_text, {color: "white"}]}>사이드 메뉴</Text>
+                    </View>
+                    <View style={{...styles.menu_view, flex:2.2}}>
+
+                        <Menu name="사이다" cost={2000}/>
+                    </View>
                 </Card>
             </View>
         </View>
@@ -136,6 +143,21 @@ const styles = StyleSheet.create({
         width: circle_size,
         borderRadius: circle_size * 2,
         backgroundColor: "#2E2E2E"
+    },
+    title_view: {
+        height: "100%",
+        width: "100%",
+        borderBottomColor: "white",
+        borderBottomWidth: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 20
+    },
+    menu_view: {
+        height: "100%",
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "flex-start"        
     }
 }); 
 
