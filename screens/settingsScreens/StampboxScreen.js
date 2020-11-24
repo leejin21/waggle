cf9bb19a21f2fe77dceed4ea1fc9e69653bbd265
Jin Lee <leejin21c@gmail.com>
2020-11-24 22:18:42 +0900

    stamp box: get

--- screens/settingsScreens/StampboxScreen.js
+++ screens/settingsScreens/StampboxScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
 
 import Colors from "../../constants/Colors";
@@ -8,12 +8,36 @@ import { headerOptions } from "../../constants/Options";
 import NoCardTemplate from "../../templates/NoCardTemplate";
 import CardButton from "../../components/CardButton"; 
 
+// custom modules for fetch
+import { par2url, getHeader } from "../../fetch/fetchApi";
+import { Context } from "../../navigation/Store";
+
 const stampDatas = [
     { name: 'ABC레스토랑', collected: '2', all: '10'},
     { name: '가나다레스토랑', collected: '1', all: '10'}
 ]
 
+const getStampBox = async (state) => {
+    const totUrl = par2url('/stamp/box', {});
+    const header = getHeader(state.userToken);
+    console.log("stamp/box GET");
+    try {
+        let response = await fetch(totUrl, {
+            method: 'GET',
+            headers: header,
+        });
+        let json = await response.json();
+        console.log('GET /STAMP/BOX');
+        console.log(json);
+        return json;
+    } catch (e) {
+        console.error(e);
+    }
+    
+};
+
 const StampboxView = (props) => {
+    // props: stampBox
     const navi = props.navigation;
 
     const renderItem = ({item}) => (
@@ -22,14 +46,21 @@ const StampboxView = (props) => {
 
     return(
         <View style={{ width: "100%" }}>
-            <Text style={{...CommonStyles.small_text, paddingVertical: 20}}>클릭시 상세 정보를 알 수 있어요.</Text>
-            <FlatList
-                key="_"
-                data={stampDatas}
-                renderItem={renderItem}
-                keyExtractor={(item, index) => index.toString()}
-                style={{ width: "100%" }}
-            ></FlatList>
+            {props.stampBox.length === 0
+                ?
+                <Text style={{...CommonStyles.small_text, paddingVertical: 20}}>쿠폰함에서 리뷰를 남기고 스탬프를 모아 주세요.</Text>
+                :
+                <View>
+                    <Text style={{...CommonStyles.small_text, paddingVertical: 20}}>클릭시 상세 정보를 알 수 있어요.</Text>
+                    <FlatList
+                        key="_"
+                        data={props.stampBox}
+                        renderItem={renderItem}
+                        keyExtractor={(item, index) => index.toString()}
+                        style={{ width: "100%" }}
+                    ></FlatList>
+                </View>
+            }
         </View>
     );
 }
@@ -47,12 +78,22 @@ const StampboxScreen = (props) => {
             color: "white",
         },
     });
-
+    const [stampBox, setStampBox] = React.useState([]);
+    const [AuthState, authDispatch] = React.useContext(Context);
     const navi = props.navigation; //?
 
+    useEffect(()=> {
+        const fetchStampBox = async () => {
+            const json = await getStampBox(AuthState);
+            // await getStampBox(AuthState);
+            await setStampBox(json);
+        };
+        fetchStampBox();
+    }, []);
+    
     return (
         <NoCardTemplate
-        bodyview={<StampboxView navigation={navi}/>}
+        bodyview={<StampboxView navigation={navi} stampBox={stampBox}/>}
         needButton={false}
         isHeaderBlack={false}
         />
